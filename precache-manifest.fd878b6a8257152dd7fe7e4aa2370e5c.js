a8b3139d6df7086da6df66ee5b80bdc2142a9b30
Minong <minhk3259@gmail.com>
2019-10-14 00:05:20 +0900

    Updates

---
+++ precache-manifest.fd878b6a8257152dd7fe7e4aa2370e5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7d6babbc0b9a7544d1b8863493afa9",
+    "url": "/color-palette/index.html"
+  },
+  {
+    "revision": "8366967c5ff31dd3c7a6",
+    "url": "/color-palette/static/css/main.4660ce60.chunk.css"
+  },
+  {
+    "revision": "9b08c9cf3284946717cf",
+    "url": "/color-palette/static/js/2.c97b69cb.chunk.js"
+  },
+  {
+    "revision": "8366967c5ff31dd3c7a6",
+    "url": "/color-palette/static/js/main.d692d4ba.chunk.js"
+  },
+  {
+    "revision": "d514cdfb580336009970",
+    "url": "/color-palette/static/js/runtime-main.b19cc67a.js"
+  }
+]);
